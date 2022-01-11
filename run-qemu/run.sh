df7a47d5be909c8cc52d9d4b6343b54d13487c14
Yucong Sun <sunyucong@gmail.com>
2022-01-10 16:06:57 -0800

    Increase qemu memory to 6G

--- run-qemu/run.sh
+++ run-qemu/run.sh
@@ -37,7 +37,7 @@ else
   accel=$tcg_accel
 fi
 "$qemu" -nodefaults --no-reboot -display none -serial mon:stdio \
-  ${accel} -smp "$smp" -m 4G \
+  ${accel} -smp "$smp" -m 6G \
   -drive file="$IMG",format=raw,index=1,media=disk,if=virtio,cache=none \
   -kernel "$vmlinuz" -append "root=/dev/vda rw console=$console panic=-1 sysctl.vm.panic_on_oom=1 $APPEND"
 
