e37f65e4f2cb4c947c9176111b852d6e2c2cabc6
Yucong Sun <sunyucong@gmail.com>
2022-02-04 17:34:34 -0500

    Update to clang15

--- setup-build-env/install_clang.sh
+++ setup-build-env/install_clang.sh
@@ -7,13 +7,14 @@ source $(cd $(dirname $0) && pwd)/../helpers.sh
 travis_fold start install_clang "Installing Clang/LLVM"
 sudo apt-get install --allow-downgrades -y libc6=2.31-0ubuntu9.2
 sudo apt-get install -y g++ libelf-dev
+
+echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal main" | sudo tee -a /etc/apt/sources.list
 n=0
 while [ $n -lt 5 ]; do
   set +e && \
   wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add - && \
-  echo "deb http://apt.llvm.org/focal/ llvm-toolchain-focal main" | sudo tee -a /etc/apt/sources.list && \
   sudo apt-get update && \
-  sudo apt-get install -y clang-14 lld-14 llvm-14 && \
+  sudo apt-get install -y clang-15 lld-15 llvm-15 && \
   set -e && \
   break
   n=$(($n + 1))
