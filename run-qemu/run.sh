9f109a008c57eb33d58f000bd9878073a1b1cca1
Daniel Müller <deso@posteo.net>
2022-08-29 10:41:46 -0700

    Uppercase vmlinuz variable
    We typically used fully uppercased names for environment variables in
    shell scripts. There does not seem to be a good reason for diverging for
    $vmlinuz specifically. Rename it.
    
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- run-qemu/run.sh
+++ run-qemu/run.sh
@@ -42,7 +42,7 @@ fi
   -serial chardev:char0 \
   ${accel} -smp "$smp" -m 6G \
   -drive file="$IMG",format=raw,index=1,media=disk,if=virtio,cache=none \
-  -kernel "$vmlinuz" -append "root=/dev/vda rw console=$console panic=-1 sysctl.vm.panic_on_oom=1 $APPEND"
+  -kernel "$VMLINUZ" -append "root=/dev/vda rw console=$console panic=-1 sysctl.vm.panic_on_oom=1 $APPEND"
 
 exitfile="$(cat $GITHUB_WORKSPACE/bpftool_exitstatus)\n"
 exitfile+="$(guestfish --ro -a "$IMG" -i cat /exitstatus 2>/dev/null)"
