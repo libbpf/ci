e87dff719e396dfda7bffc682cf4c4a9dd18dbc8
Song Liu <song@kernel.org>
2023-03-20 12:47:28 -0700

    Add inputs.llvm-version
    Since we always build BPF programs with clang, ci also installs llvm when
    inputs.toolchain is gcc. llvm_version(). Instead of parsing llvm version
    from inputs.toolchain, add a separate input field of llvm-version.
    
    Signed-off-by: Song Liu <song@kernel.org>

--- build-samples/build_samples.sh
+++ build-samples/build_samples.sh
@@ -10,9 +10,9 @@ KERNEL="$1"
 TOOLCHAIN="$2"
 export KBUILD_OUTPUT="$3"
 
-LLVM_VER="$(llvm_version $TOOLCHAIN)" && :
-if [ $? -eq 0 ]; then
-	export LLVM="-$LLVM_VER"
+if [ $TOOLCHAIN = "llvm" ]; then
+	export LLVM="-$LLVM_VERSION"
+	TOOLCHAIN="llvm-$LLVM_VERSION"
 fi
 
 foldable start build_samples "Building samples with $TOOLCHAIN"
@@ -25,13 +25,13 @@ fi
 
 make headers_install
 make \
-	CLANG=clang-${LLVM_VER} \
-	OPT=opt-${LLVM_VER} \
-	LLC=llc-${LLVM_VER} \
-	LLVM_DIS=llvm-dis-${LLVM_VER} \
-	LLVM_OBJCOPY=llvm-objcopy-${LLVM_VER} \
-	LLVM_READELF=llvm-readelf-${LLVM_VER} \
-	LLVM_STRIP=llvm-strip-${LLVM_VER} \
+	CLANG=clang-${LLVM_VERSION} \
+	OPT=opt-${LLVM_VERSION} \
+	LLC=llc-${LLVM_VERSION} \
+	LLVM_DIS=llvm-dis-${LLVM_VERSION} \
+	LLVM_OBJCOPY=llvm-objcopy-${LLVM_VERSION} \
+	LLVM_READELF=llvm-readelf-${LLVM_VERSION} \
+	LLVM_STRIP=llvm-strip-${LLVM_VERSION} \
 	VMLINUX_BTF="${KBUILD_OUTPUT}/vmlinux" \
 	VMLINUX_H="${VMLINUX_H}" \
 	-C "${REPO_ROOT}/${REPO_PATH}/samples/bpf" \
