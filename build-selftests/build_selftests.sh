f1b373bbffe471cf9bc295c68d0d64a9c1843c01
Manu Bretelle <chantr4@gmail.com>
2024-02-12 18:12:51 -0800

    samples/selftest: support cross-compilation
    Similarly to the kernel, support cross-compilation.
    We now need to pass the architecture when invoking the build script.
    
    Signed-off-by: Manu Bretelle <chantr4@gmail.com>

--- build-selftests/build_selftests.sh
+++ build-selftests/build_selftests.sh
@@ -6,9 +6,18 @@ THISDIR="$(cd $(dirname $0) && pwd)"
 
 source "${THISDIR}"/../helpers.sh
 
-KERNEL="$1"
-TOOLCHAIN="$2"
-export KBUILD_OUTPUT="$3"
+TARGET_ARCH="$1"
+KERNEL="$2"
+TOOLCHAIN="$3"
+export KBUILD_OUTPUT="$4"
+
+ARCH="$(platform_to_kernel_arch ${TARGET_ARCH})"
+CROSS_COMPILE=""
+
+if [[ "${TARGET_ARCH}" != "$(uname -m)" ]]
+then
+	CROSS_COMPILE="${TARGET_ARCH}-linux-gnu-"
+fi
 
 if [[ $TOOLCHAIN = "llvm" ]]; then
 	export LLVM="-$LLVM_VERSION"
@@ -33,6 +42,8 @@ fi
 cd ${REPO_ROOT}/${REPO_PATH}
 
 MAKE_OPTS=$(cat <<EOF
+	ARCH=${ARCH}
+	CROSS_COMPILE=${CROSS_COMPILE}
 	CLANG=clang-${LLVM_VERSION}
 	LLC=llc-${LLVM_VERSION}
 	LLVM_STRIP=llvm-strip-${LLVM_VERSION}
