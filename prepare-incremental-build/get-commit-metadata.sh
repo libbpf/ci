77c8e0eb0763bc00d779c7997552c96c319256b7
Ihor Solodrai <ihor.solodrai@pm.me>
2024-09-16 18:01:19 -0700

    prepare-incremental-build: update inputs and get-commit-metadata.sh
    Make `kbuild-output` a required input.  Pass an absolute path to
    `kbuild-output` from kernel-build reusable workflow.
    
    Introduce optional `cache-key-prefix` input.
    
    Remove branch name parsing logic from `get-commit-metadata.sh`.
    Accept the branch that is a target for caching as argument.

--- prepare-incremental-build/get-commit-metadata.sh
+++ prepare-incremental-build/get-commit-metadata.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-# branch="${GITHUB_BASE_REF}"
-
-# if [ "${GITHUB_EVENT_NAME}" = 'push' ]; then
-#   branch="${GITHUB_REF_NAME}"
-# fi
-
 set -eux
 
-branch=${1:-bpf-next_base}
+branch=$1
 
 echo "branch=${branch}" >> "${GITHUB_OUTPUT}"
 
-upstream="${branch//_base/}"
-
 git fetch --quiet --prune --no-tags --depth=1 --no-recurse-submodules \
-    origin "+refs/heads/${upstream}:refs/remotes/origin/${upstream}"
-commit=$(git rev-parse "origin/${upstream}")
+    origin "+refs/heads/${branch}:refs/remotes/origin/${branch}"
+commit=$(git rev-parse "origin/${branch}")
 
 timestamp_utc="$(TZ=utc git show --format='%cd' --no-patch --date=iso-strict-local "${commit}")"
 
 echo "timestamp=${timestamp_utc}" >> "${GITHUB_OUTPUT}"
 echo "commit=${commit}" >> "${GITHUB_OUTPUT}"
-echo "Most recent upstream commit is ${commit}"
+echo "Most recent ${branch} commit is ${commit}"
 
