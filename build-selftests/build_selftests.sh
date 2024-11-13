ee740c2b3176b2b531c2b78b33a764b8f16f47d7
Ihor Solodrai <isolodrai@meta.com>
2024-11-13 14:13:26 -0800

    build-selftests: v2 of the action inputs
    Changes in action inputs:
      * kernel-root is now a required input
      * kernel, kbuild-output and max-make-jobs are removed
    
    kernel-root input is required, because to build selftests
    one must to clone a kernel source tree first.
    
    The build script behavior can be directed more granularly with env
    variables:
      * KBUILD_OUTPUT now defaults to the kernel-root
      * VMLINUX_BTF now can be set externally
      * MAX_MAKE_JOBS can be set as env variable if desired
    
    Preparation script logic is removed: the rationale is that the caller
    is responsible for setting up required environment before building.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@pm.me>

--- build-selftests/build_selftests.sh
+++ build-selftests/build_selftests.sh
@@ -5,10 +5,12 @@ set -euo pipefail
 source "${GITHUB_ACTION_PATH}/../helpers.sh"
 
 TARGET_ARCH="$1"
-KERNEL="$2"
-TOOLCHAIN="$3"
-export KBUILD_OUTPUT="$4"
-VMLINUX_H=${VMLINUX_H:-}
+TOOLCHAIN="$2"
+KERNEL_ROOT="$(realpath $3)"
+
+export KBUILD_OUTPUT="${KBUILD_OUTPUT:-${KERNEL_ROOT}}"
+export VMLINUX_BTF="${VMLINUX_BTF:-${KBUILD_OUTPUT}/vmlinux}"
+export VMLINUX_H="${VMLINUX_H:-}"
 
 ARCH="$(platform_to_kernel_arch ${TARGET_ARCH})"
 CROSS_COMPILE=""
@@ -25,18 +27,13 @@ fi
 
 foldable start build_selftests "Building selftests with $TOOLCHAIN"
 
-PREPARE_SELFTESTS_SCRIPT=$(find $GITHUB_WORKSPACE -name prepare_selftests-${KERNEL}.sh)
-if [ -f "${PREPARE_SELFTESTS_SCRIPT}" ]; then
-	(cd "${KERNEL_ROOT}/tools/testing/selftests/bpf" && ${PREPARE_SELFTESTS_SCRIPT})
-fi
-
 MAKE_OPTS=$(cat <<EOF
 	ARCH=${ARCH}
 	CROSS_COMPILE=${CROSS_COMPILE}
 	CLANG=clang-${LLVM_VERSION}
 	LLC=llc-${LLVM_VERSION}
 	LLVM_STRIP=llvm-strip-${LLVM_VERSION}
-	VMLINUX_BTF=${KBUILD_OUTPUT}/vmlinux
+	VMLINUX_BTF=${VMLINUX_BTF}
 	VMLINUX_H=${VMLINUX_H}
 EOF
 )
