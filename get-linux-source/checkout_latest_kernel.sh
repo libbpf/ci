9161bf0471ffadfac885e72cf701b2d8461baa76
Ihor Solodrai <ihor.solodrai@pm.me>
2024-11-18 12:44:43 -0800

    git-linux-source: handle FETCH_DEPTH=0 correctly
    Properly handle FETCH_DEPTH=0 (default) case. If snapshot download has
    failed, clone with --depth=1
    
    Refactor checkout_latest_kernel.sh and make the conditions more clear.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@pm.me>

--- get-linux-source/checkout_latest_kernel.sh
+++ get-linux-source/checkout_latest_kernel.sh
@@ -25,34 +25,45 @@ SNAPSHOT_URL=${KERNEL_ORIGIN}/snapshot/bpf-next-${LINUX_SHA}.tar.gz
 echo LINUX_SHA = ${LINUX_SHA}
 echo SNAPSHOT_URL = ${SNAPSHOT_URL}
 
-if [ ! -d "${REPO_PATH}" ]; then
-	echo
-	foldable start pull_kernel_srcs "Fetching kernel sources"
-
-	mkdir -p $(dirname "${REPO_PATH}")
-	cd $(dirname "${REPO_PATH}")
-	# attempt to fetch desired bpf-next repo snapshot
-	if [ -n "${SNAPSHOT_URL}" ] && [ "${FETCH_DEPTH}" -eq 0 ] && \
-	   wget -U 'BPFCIBot/1.0 (bpf@vger.kernel.org)' -nv ${SNAPSHOT_URL} && \
-	   tar xf bpf-next-${LINUX_SHA}.tar.gz --totals ; then
-		mv bpf-next-${LINUX_SHA} $(basename ${REPO_PATH})
-	else
-	    # but fallback to git fetch approach if that fails
-            git clone --depth ${FETCH_DEPTH} ${KERNEL_ORIGIN} ${REPO_PATH}
-            cd "${REPO_PATH}"
-	    # check if desired SHA exists
-	    if ! git cat-file -e ${LINUX_SHA}^{commit} ; then
-                # if not, fetch all of bpf-next; slow and painful
-                git fetch origin
-            fi
-            git reset --hard ${LINUX_SHA}
-            cd -
-	fi
-        if [ "${FETCH_DEPTH}" -eq 0 ]; then
-	    rm -rf ${REPO_PATH}/.git || true
-        fi
-
-	foldable end pull_kernel_srcs
-else
+if [ -d "${REPO_PATH}" ]; then
     echo "${REPO_PATH} directory already exists, will not download kernel sources"
+    exit 0
 fi
+
+mkdir -p $(dirname "${REPO_PATH}")
+cd $(dirname "${REPO_PATH}")
+
+# attempt to fetch desired bpf-next repo snapshot
+if [[ -n "${SNAPSHOT_URL}" && "${FETCH_DEPTH}" -le 0 ]]; then
+    wget -U 'BPFCIBot/1.0 (bpf@vger.kernel.org)' -nv ${SNAPSHOT_URL} || true
+    PRESERVE_DOT_GIT=
+else
+    PRESERVE_DOT_GIT=true
+fi
+
+tarball=bpf-next-${LINUX_SHA}.tar.gz
+if [[ -f "$tarball" ]]; then
+    tar xf "$tarball" --totals
+    mv bpf-next-${LINUX_SHA} $(basename ${REPO_PATH})
+else
+    # FETCH_DEPTH can be 0 here in case snapshot download has failed
+    if [ "${FETCH_DEPTH}" -le 0 ]; then
+        FETCH_DEPTH=1
+    fi
+
+    git clone --depth ${FETCH_DEPTH} ${KERNEL_ORIGIN} ${REPO_PATH}
+
+    cd "${REPO_PATH}"
+    # check if desired SHA exists
+    if ! git cat-file -e ${LINUX_SHA}^{commit} ; then
+        # if not, fetch all of bpf-next; slow and painful
+	git fetch origin
+    fi
+    git reset --hard ${LINUX_SHA}
+    cd -
+fi
+
+if [ -z "${PRESERVE_DOT_GIT}" ]; then
+    rm -rf ${REPO_PATH}/.git || true
+fi
+
