b1bc05c9a1e7538be4bf591cd2938ad27b6b45e6
Ihor Solodrai <ihor.solodrai@pm.me>
2025-01-10 11:39:03 -0800

    run-vmtest: introduce run-vmtest.env
    Update run-vmtest action to source $VMTEST_CONFIGS/run-vmtest.env when
    present. For detais read a comment in ci/vmtest/configs/run-vmtest.env
    
    Add scripts local to run-vmtest that handle the merge of multiple
    allow/denylists for BPF selftests.
    
    Passing @filename as allow/denylist to test runners, instead of
    inlining comma-separated test names.
    
    Add test_progs-bpf_gcc as a possible test runner in
    run-vmtest/run-bpf-selftests.sh
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@pm.me>

--- run-vmtest/run.sh
+++ run-vmtest/run.sh
@@ -40,13 +40,20 @@ then
     RUN_BPFTOOL_CHECKS=true
 fi
 
-VMTEST_SCRIPT=${VMTEST_SCRIPT:-}
-if [[ -z "$VMTEST_SCRIPT" \
-          && "$KERNEL_TEST" != "sched_ext" ]];
+VMTEST_CONFIGS=${VMTEST_CONFIGS:-}
+if [[ -n "$VMTEST_CONFIGS" && -f "${VMTEST_CONFIGS}/run-vmtest.env" ]];
 then
-	VMTEST_SCRIPT="${GITHUB_ACTION_PATH}/run-bpf-selftests.sh"
-else
-	VMTEST_SCRIPT="${GITHUB_ACTION_PATH}/run-scx-selftests.sh"
+    source "${VMTEST_CONFIGS:-}/run-vmtest.env"
+fi
+
+VMTEST_SCRIPT=${VMTEST_SCRIPT:-}
+if [[ -z "$VMTEST_SCRIPT" && "$KERNEL_TEST" == "sched_ext" ]];
+then
+    VMTEST_SCRIPT="${GITHUB_ACTION_PATH}/run-scx-selftests.sh"
+elif [[ -z "$VMTEST_SCRIPT" ]];
+then
+    ${GITHUB_ACTION_PATH}/prepare-bpf-selftests.sh
+    VMTEST_SCRIPT="${GITHUB_ACTION_PATH}/run-bpf-selftests.sh"
 fi
 
 # clear exitstatus file
