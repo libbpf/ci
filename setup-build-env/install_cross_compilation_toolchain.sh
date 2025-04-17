98daecacb7d1f669c80a315a33a62adf53eab6d4
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-04-17 11:41:19 -0700

    setup-build-env: fix cross-compilation dependencies
    BPF CI got broken due to apt errors when installing cross compilers:
    * https://github.com/kernel-patches/bpf/actions/runs/14519065074/job/40735249827
    
    dpkg --add-architecture now breaks apt config. Looks like there were
    changes in Ubuntu that require migrating to deb822 [1][2] format of
    apt package lists.
    
    In our case not only target arch repos need to be added, but also
    default /etc/apt/sources.list.d/ubuntu.sources needs to be modified.
    The host architecture has to be whitelisted, otherwise apt attempts
    fetching target arch packages from archive and security, and they are
    not there.
    
    Another issue was with libelf-dev:s390x dependencies. Current
    linux-libc-dev breaks linux-libc-dev:s390x, and unfortunately we need
    both for a proper cross-compilation build. Using aptituide I found
    that downgrading linux-libc-dev:amd64 resolves this conflict.
    
    [1] actions/runner-images#10901
    [2] https://discourse.ubuntu.com/t/ubuntu-24-04-lts-noble-numbat-release-notes/39890#p-99950-deb822-sources-management
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- setup-build-env/install_cross_compilation_toolchain.sh
+++ setup-build-env/install_cross_compilation_toolchain.sh
@@ -4,28 +4,62 @@ set -euo pipefail
 
 THISDIR="$(cd "$(dirname "$0")" && pwd)"
 
-# shellcheck source=./helpers.sh
 source "${THISDIR}"/../helpers.sh
 
 TARGET_ARCH="$1"
 
 foldable start install_crosscompile "Installing Cross-Compilation toolchain"
 
-if [[ "${TARGET_ARCH}" != "$(uname -m)" ]]
-then
-	DEB_ARCH="$(platform_to_deb_arch "${TARGET_ARCH}")"
-	# shellcheck source=/dev/null
-	. /etc/os-release
-	cat <<EOF > /etc/apt/sources.list.d/xcompile.list
-deb [arch=${DEB_ARCH}] http://ports.ubuntu.com/ubuntu-ports  ${VERSION_CODENAME} main restricted
-deb [arch=${DEB_ARCH}] http://ports.ubuntu.com/ubuntu-ports  ${VERSION_CODENAME}-updates main restricted
-EOF
-	apt update
-	# Add the architecture
-	dpkg --add-architecture "${DEB_ARCH}"
-	apt install -y g{cc,++}-"${TARGET_ARCH}-linux-gnu" {libelf-dev,libssl-dev}:"${DEB_ARCH}"
-else
+if [[ "${TARGET_ARCH}" == "$(uname -m)" ]]; then
     echo "Nothing to do. Target arch is the same as host arch: ${TARGET_ARCH}"
+    exit 0
 fi
+
+source /etc/os-release
+
+DEB_ARCH="$(platform_to_deb_arch "${TARGET_ARCH}")"
+DEB_HOST_ARCH="$(dpkg --print-architecture)"
+UBUNTU_CODENAME=${VERSION_CODENAME:-noble}
+
+cat <<EOF | sudo tee /etc/apt/sources.list.d/ubuntu.sources
+Types:        deb
+URIs:         http://archive.ubuntu.com/ubuntu/
+Suites:       ${UBUNTU_CODENAME} ${UBUNTU_CODENAME}-updates ${UBUNTU_CODENAME}-backports
+Components:   main restricted universe multiverse
+Architectures:   ${DEB_HOST_ARCH}
+Signed-By:    /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+Types:        deb
+URIs:         http://security.ubuntu.com/ubuntu/
+Suites:       ${UBUNTU_CODENAME}-security
+Components:   main restricted universe multiverse
+Architectures:   ${DEB_HOST_ARCH}
+Signed-By:    /usr/share/keyrings/ubuntu-archive-keyring.gpg
+EOF
+
+sudo dpkg --add-architecture "$DEB_ARCH"
+cat <<EOF | sudo tee /etc/apt/sources.list.d/xcompile.sources
+Types: deb
+URIs: http://ports.ubuntu.com/ubuntu-ports
+Suites: ${UBUNTU_CODENAME} ${UBUNTU_CODENAME}-updates ${UBUNTU_CODENAME}-security
+Components: main restricted universe multiverse
+Architectures: ${DEB_ARCH}
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+EOF
+
+sudo apt-get update -y
+
+# downgrade due to conflict with linux-libc-dev:s390x
+sudo apt-get install --allow-downgrades -y linux-libc-dev=6.8.0-57.59
+
+sudo apt-get install -y                  \
+     "crossbuild-essential-${DEB_ARCH}"  \
+     "binutils-${TARGET_ARCH}-linux-gnu" \
+     "gcc-${TARGET_ARCH}-linux-gnu"      \
+     "g++-${TARGET_ARCH}-linux-gnu"      \
+     "linux-libc-dev:${DEB_ARCH}"        \
+     "libelf-dev:${DEB_ARCH}"            \
+     "libssl-dev:${DEB_ARCH}"            \
+     "zlib1g-dev:${DEB_ARCH}"
 
 foldable end install_crosscompile
