3e67ab608f6868e7f2fe019343f7b5ac6d6f8def
Ihor Solodrai <isolodrai@meta.com>
2025-06-04 18:29:49 -0700

    run-vmtest: factor out install-dependencies.sh
    Move dependencies setup from action.yml to a shell script. Add
    iproute2 and friends to the list of installed packages.
    
    Signed-off-by: Ihor Solodrai <isolodrai@meta.com>

---
+++ run-vmtest/install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euo pipefail
+
+VMTEST_RELEASE=${VMTEST_RELEASE:-v0.15.0}
+VMTEST_URL="https://github.com/danobi/vmtest/releases/download/${VMTEST_RELEASE}/vmtest-$(uname -m)"
+sudo curl -L $VMTEST_URL -o /usr/bin/vmtest
+sudo chmod 755 /usr/bin/vmtest
+
+sudo apt-get update -y
+sudo -E apt-get install --no-install-recommends -y \
+     cpu-checker ethtool gawk iproute2 iptables iputils-ping keyutils libpcap-dev
+sudo -E apt-get install --no-install-recommends -y \
+     qemu-guest-agent qemu-kvm qemu-system-arm qemu-system-s390x qemu-system-x86 qemu-utils
