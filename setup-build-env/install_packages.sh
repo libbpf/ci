3cbaa9f091e624139ec2d1b44cb8ef1a18685505
Ihor Solodrai <ihor.solodrai@linux.dev>
2025-10-17 12:55:08 -0700

    setup-build-env: enable GCC 15 installation
    GCC 15 is not available in Ubuntu 24.04 repositories, which is the
    distro used by both github and self-hosted BPF CI runners.
    
    To enable GCC 15 installation, make setup-build-env scripts set up
    Ubuntu 25.10 (questing) repositories and then install the packages.
    
    Also update cross-compilation setup with a apt config restrictions
    instead of manipulating the source lists.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- setup-build-env/install_packages.sh
+++ setup-build-env/install_packages.sh
@@ -10,14 +10,34 @@ export GCC_VERSION=${GCC_VERSION:-14}
 
 foldable start install_packages
 
+# gcc >= 15 is not available in Ubuntu 24, so add Ubuntu 25 repositories
+if [ "$GCC_VERSION" -ge 15 ]; then
+    UBUNTU_CODENAME=questing # Ubuntu 25.10 with GCC 15.2
+    cat <<EOF | sudo tee /etc/apt/sources.list.d/${UBUNTU_CODENAME}.list
+deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME} main universe
+deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME}-updates main universe
+deb http://archive.ubuntu.com/ubuntu ${UBUNTU_CODENAME}-security main universe
+EOF
+    cat <<EOF | sudo tee /etc/apt/preferences.d/${UBUNTU_CODENAME}
+Package: *
+Pin: release n=${UBUNTU_CODENAME}
+Pin-Priority: 999
+EOF
+fi
+
+sudo apt-get update -y
+
+# add git-core/ppa to install latest git version
+sudo -E apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:git-core/ppa
 sudo apt-get update -y
 
 sudo -E apt-get install --no-install-recommends -y                     \
      bc bison build-essential cmake cpu-checker curl dumb-init         \
      elfutils ethtool ethtool flex gawk git iproute2 iptables          \
      iputils-ping jq keyutils libguestfs-tools pkg-config              \
-     python3-docutils python3-minimal rsync software-properties-common \
-     sudo texinfo tree tzdata wget xxd xz-utils zstd
+     python3-docutils python3-minimal rsync sudo texinfo tree          \
+     tzdata wget xxd xz-utils zstd
 
 sudo -E apt-get install --no-install-recommends -y            \
      binutils-dev libcap-dev libdw-dev libelf-dev libpcap-dev \
