423ac37927d0042db9eec87dedb691e74c65c1de
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-01-21 17:05:23 -0800

    setup-build-env: Make sure llvm-dev is installed
    There BPF selftests that depend on llvm-dev libraries. Make sure they
    are installed (by specifying "all" to llvm.sh), and ensure LLVM_CONFIG
    is set correctly when building selftests.
    
    Signed-off-by: Ihor Solodrai <ihor.solodrai@linux.dev>

--- setup-build-env/action.sh
+++ setup-build-env/action.sh
@@ -7,7 +7,7 @@ export GITHUB_ACTION_PATH=${GITHUB_ACTION_PATH:-$(pwd)}
 export PAHOLE_BRANCH=${PAHOLE_BRANCH:-master}
 export PAHOLE_ORIGIN=${PAHOLE_ORIGIN:-https://git.kernel.org/pub/scm/devel/pahole/pahole.git}
 export GCC_VERSION=${GCC_VERSION:-15}
-export LLVM_VERSION=${LLVM_VERSION:-20}
+export LLVM_VERSION=${LLVM_VERSION:-21}
 export TARGET_ARCH=${TARGET_ARCH:-$(uname -m)}
 
 # gcc >= 15 is not available in Ubuntu 24
