28e8b26237896b473c7321e44f311e75df7885de
Ihor Solodrai <ihor.solodrai@linux.dev>
2026-02-03 10:40:17 -0800

    setup-build-env: fixup LLVM installation script for recent Debians
    TL;DR Don't use llvm.sh on Debian.
    
    There are a couple of issues with using official llvm.sh installation
    script on recent debians. The first one is this:
    
        61.12 W: OpenPGP signature verification failed:
        https://apt.llvm.org/unstable llvm-toolchain-21 InRelease: Sub-process
        /usr/bin/sqv returned an error code (1), error message is: Signing key
        on 6084F3CF814B57C1CF12EFD515CF4D18AF4F7421 is not bound:
        No binding signature at time 2025-12-21T23:13:57Z   because: Policy
        rejected non-revocation signature (PositiveCertification) requiring
        second pre-image resistance   because: SHA1 is not considered secure
        since 2026-02-01T00:00:00Z
    
    Which requires updates on LLVM distribution side.
    
    The second is that debian removed software-properties-common package
    due to a bug [1].
    
    All this is unimportant on Debian though, because LLVM 21 is available
    in default debian PPAs. So simply install llvm packages directly, if
    we are on Debian.
    
    [1] https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1038747

--- setup-build-env/install_clang.sh
+++ setup-build-env/install_clang.sh
@@ -6,12 +6,44 @@ source "${THISDIR}"/../helpers.sh
 
 foldable start install_clang "Install LLVM ${LLVM_VERSION}"
 
-sudo apt-get update -y
-sudo -E apt-get install --no-install-recommends -y \
-     gnupg lsb-release software-properties-common wget
+source /etc/os-release
 
-curl -O https://apt.llvm.org/llvm.sh
-chmod +x llvm.sh
-sudo ./llvm.sh ${LLVM_VERSION} all
+if [[ "${ID}" == "ubuntu" ]]; then
+    # Use official installation script for Ubuntu
+    sudo apt-get update -y
+    sudo -E apt-get install --no-install-recommends -y \
+        curl gnupg lsb-release software-properties-common wget
+    curl -O https://apt.llvm.org/llvm.sh
+    chmod +x llvm.sh
+    sudo ./llvm.sh ${LLVM_VERSION} all
+elif [[ "${ID}" == "debian" ]]; then
+    # For Debian, install packages directly from repos
+    # Recent debian considers SHA1 insecure, and llvm.sh hasn't been fixed yet
+    # Install packages direcctly from repos, assuming LLVM_VERSION is available
+    sudo apt-get update -y
+    sudo -E apt-get install --no-install-recommends -y \
+        clang-${LLVM_VERSION} \
+        lldb-${LLVM_VERSION} \
+        lld-${LLVM_VERSION} \
+        clangd-${LLVM_VERSION} \
+        clang-tidy-${LLVM_VERSION} \
+        clang-format-${LLVM_VERSION} \
+        clang-tools-${LLVM_VERSION} \
+        llvm-${LLVM_VERSION}-dev \
+        llvm-${LLVM_VERSION}-tools \
+        libomp-${LLVM_VERSION}-dev \
+        libc++-${LLVM_VERSION}-dev \
+        libc++abi-${LLVM_VERSION}-dev \
+        libclang-common-${LLVM_VERSION}-dev \
+        libclang-${LLVM_VERSION}-dev \
+        libclang-cpp${LLVM_VERSION}-dev \
+        liblldb-${LLVM_VERSION}-dev \
+        libunwind-${LLVM_VERSION}-dev \
+        libclang-rt-${LLVM_VERSION}-dev \
+        libpolly-${LLVM_VERSION}-dev
+else
+     echo "$(basename "$0") unexpected distro: ${ID}" >&2
+     exit 1
+fi
 
 foldable end install_clang
